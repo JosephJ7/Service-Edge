97d36670454fd8ea36122f16302822b29db358a6
JosephJ7 <josephjacobj2001@gmail.com>
2021-08-20 14:37:16 +0530

    First Project

---
+++ src/service/edge/MyConnection.java
@@ -0,0 +1,27 @@
+package service.edge;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author joseph
+ */
+public class MyConnection {
+    
+    public static Connection getConnection(){
+     
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_login_register", "root", "28041965@Jose");
+            System.out.println("Succesfull");
+//            jdbc:mysql://localhost:3306/?user=root
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        return con;
+    }
+}
